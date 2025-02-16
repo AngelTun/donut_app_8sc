2e3c6ea771af714d3356a2e81b365df9dc031a78
AngelTun <159564191+AngelTun@users.noreply.github.com>
2025-02-16 14:32:18 -0600

    DonutTile finished

--- lib/utils/donut_tile.dart
+++ lib/utils/donut_tile.dart
@@ -45,9 +45,59 @@ class DonutTile extends StatelessWidget {
             //Imagen del producto
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 12),
-              child: Image.asset(imageName),)
+              //En esta parte le tuve que agregar el sizedBox, ya que la imagen era muy grande, 
+              // y esto hacia que me saliera unas lineas de cebra diciendo que se paso del limite, y ya con esto ajuste sus tamaños verticales y horizontales
+              child: SizedBox( 
+                width: 400,
+                height: 400,
+                child: Image.asset(imageName),),),
             //Texto del  sabor del producto con la tienda
+            // Texto del sabor del producto
+            Text(
+              donutFlavor,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 4),
+            // Nombre de la tienda
+            const Text(
+              'Crispy Cream',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+            const SizedBox(height: 4),
             //íconos de "Me encanta" y "agregar"
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // Ícono de corazón
+                  Icon(
+                    Icons.favorite_border,
+                    size: 28,
+                    color: Colors.black, 
+                  ),
+                  SizedBox(width: 4), // Espacio entre elementos
+                  // Texto "Add"
+                  Text(
+                    "Agregar",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
           ],),
       ),
     );
